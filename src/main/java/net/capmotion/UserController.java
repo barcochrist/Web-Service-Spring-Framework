6e674c80ff12745fbb68437f3d496e2be2690d17
barcochrist <barcochrist@gmail.com>
2018-04-07 22:28:10 -0500

    implementation functionality add user
    creation of method to create a user

--- src/main/java/net/capmotion/UserController.java
+++ src/main/java/net/capmotion/UserController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +23,24 @@ public class UserController {
 		logger.info("getting all users");
 		return userRepository.findAll();
 	}
+
+	@GetMapping(path = "/add")
+	public @ResponseBody String addUser(@RequestParam(value = "firstName", required = true) String firstName,
+			@RequestParam(value = "lastName", required = true) String lastName,
+			@RequestParam(value = "userName", required = true) String userName,
+			@RequestParam(value = "email", required = true) String email,
+			@RequestParam(value = "phoneNumber", required = false) String phoneNumber,
+			@RequestParam(value = "password", required = true) String password) {
+
+		User user = new User();
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setUserName(userName);
+		user.setEmail(email);
+		user.setPhoneNumber(phoneNumber);
+		user.setPassword(password);
+		userRepository.save(user);
+		return "User created";
+	}
+
 }
